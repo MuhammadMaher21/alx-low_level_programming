960e8129a1a3d4c94edc604ccb51df2b13ca65c3
muhammad <mohamed.maher.eisa20@gmail.com>
2022-09-09 12:46:14 -0700

    Coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
 * main - print the string in the put function
-*
+* 
 * Description: using the main function
-* this program prints "Programming is like a multilingual puzzle
-* Return: 0
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 00
 */
 int main(void)
 {
-       puts("\"Programming is like a multinlingual puzzle");
+       puts("\"Programming is like building a multilingual puzzle");
        return (0);
 }
